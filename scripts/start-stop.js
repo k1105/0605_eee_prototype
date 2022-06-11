e464aef66afcaa1f1eeea52babefb5782420901b
k1105 <kanapon1105@yahoo.co.jp>
2022-06-11 14:38:49 +0900

    vibration ratioの出力

--- scripts/start-stop.js
+++ scripts/start-stop.js
@@ -1,4 +1,5 @@
 let vibration = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+let vibration_ratio_value = 0;
 let time = 0;
 let walking_scalar = 1.0;
 let top_threshold = 1.05;
@@ -13,6 +14,7 @@ const top_threshold_slider = document.getElementById("topThresholdSlider");
 const bottom_threshold_slider = document.getElementById(
   "bottomThresholdSlider"
 );
+const vibration_ratio = document.getElementById("vibrationRatio");
 
 setInterval(() => {
   if (walking_flag == 1) {
@@ -20,12 +22,13 @@ setInterval(() => {
   } else if (walking_flag == 0) {
     elem.style.backgroundColor = "#ff0000";
   } else {
-    elem.style.backgroundColor = "#000000";
+    elem.style.backgroundColor = "#dddddd";
   }
   top_threshold_value.innerHTML = top_threshold_slider.value / 100;
   bottom_threshold_value.innerHTML = bottom_threshold_slider.value / 100;
   top_threshold = top_threshold_slider.value / 100;
   bottom_threshold = bottom_threshold_slider.value / 100;
+  vibration_ratio.innerHTML = vibration_ratio_value;
 }, 100);
 
 window.addEventListener(
@@ -45,31 +48,21 @@ window.addEventListener(
     }
 
     if (time > 9) {
+      vibration_ratio_value =
+        (vibration[0] +
+          vibration[1] +
+          vibration[2] +
+          vibration[3] +
+          vibration[4]) /
+        (vibration[5] +
+          vibration[6] +
+          vibration[7] +
+          vibration[8] +
+          vibration[9] +
+          0.000001);
       if (
-        (vibration[0] +
-          vibration[1] +
-          vibration[2] +
-          vibration[3] +
-          vibration[4]) /
-          (vibration[5] +
-            vibration[6] +
-            vibration[7] +
-            vibration[8] +
-            vibration[9] +
-            0.000001) >
-          top_threshold ||
-        (vibration[0] +
-          vibration[1] +
-          vibration[2] +
-          vibration[3] +
-          vibration[4]) /
-          (vibration[5] +
-            vibration[6] +
-            vibration[7] +
-            vibration[8] +
-            vibration[9] +
-            0.000001) <
-          bottom_threshold
+        vibration_ratio_value > top_threshold ||
+        vibration_ratio_value < bottom_threshold
       ) {
         walking_flag = 1;
       } else {
