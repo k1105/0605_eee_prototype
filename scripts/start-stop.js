928ca812d524deffa166b21daae2b12642a79752
k1105 <kanapon1105@yahoo.co.jp>
2022-06-11 13:21:23 +0900

    経時的な変化のグラフ化

--- scripts/start-stop.js
+++ scripts/start-stop.js
@@ -1,17 +1,28 @@
-// function setup() {
-//   //キャンバスを作成
-//   createCanvas(window.windowWidth, window.windowHeight);
-//   //背景色
-//   //オブジェクトの色
-//   noFill();
-//   strokeWeight(10);
-// }
+function setup() {
+  //キャンバスを作成
+  createCanvas(window.windowWidth, window.windowHeight);
+  //背景色
+  //オブジェクトの色
+  noFill();
+  strokeWeight(10);
+}
 
-// function draw() {
-//   //notify death
+function draw() {
+  //notify death
 
-//   background(0);
-// }
+  background(255);
+  for (let i = 0; i < 100; i++) {
+    point(i * 10, 300 - xs[i]);
+    point(i * 10, 400 - ys[i]);
+    point(i * 10, 500 - zs[i]);
+    point(i * 10, 600 - x_orientations[i]);
+    point(i * 10, 700 - y_orientations[i]);
+    point(i * 10, 800 - z_orientations[i]);
+    point(i * 10, 900 - x_degrees[i]);
+    point(i * 10, 1000 - y_degrees[i]);
+    point(i * 10, 1100 - z_degrees[i]);
+  }
+}
 
 const elem = document.getElementById("innerHTMLtxt");
 let x,
@@ -23,7 +34,18 @@ let x,
   x_degree,
   y_degree,
   z_degree;
-let hoge = 0;
+
+let t = 0;
+
+const xs = new Array(100).fill(0);
+const ys = new Array(100).fill(0);
+const zs = new Array(100).fill(0);
+const x_orientations = new Array(100).fill(0);
+const y_orientations = new Array(100).fill(0);
+const z_orientations = new Array(100).fill(0);
+const x_degrees = new Array(100).fill(0);
+const y_degrees = new Array(100).fill(0);
+const z_degrees = new Array(100).fill(0);
 
 window.addEventListener(
   "devicemotion",
@@ -50,7 +72,45 @@ window.addEventListener(
 );
 
 setInterval(() => {
-  hoge++;
+  if (typeof x !== "undefined") {
+    xs.push(x);
+    xs.shift();
+  }
+  if (typeof y !== "undefined") {
+    ys.shift();
+    ys.push(y);
+  }
+  if (typeof z !== "undefined") {
+    zs.shift();
+    zs.push(z);
+  }
+
+  if (typeof x_orientation !== "undefined") {
+    x_orientations.shift();
+    x_orientations.push(x_orientation);
+  }
+  if (typeof y_orientation !== "undefined") {
+    y_orientations.shift();
+    y_orientations.push(y_orientation);
+  }
+  if (typeof z_orientation !== "undefined") {
+    z_orientations.shift();
+    z_orientations.push(z_orientation);
+  }
+
+  if (typeof x_degree !== "undefined") {
+    x_degrees.shift();
+    x_degrees.push(x_degree);
+  }
+  if (typeof y_degree !== "undefined") {
+    y_degrees.shift();
+    y_degrees.push(y_degree);
+  }
+  if (typeof z_degree !== "undefined") {
+    z_degrees.shift();
+    z_degrees.push(z_degree);
+  }
+
   elem.innerHTML =
     "accelerationIncludingGravity.x: " +
     x +
@@ -78,9 +138,7 @@ setInterval(() => {
     "<br />" +
     "z_degree: " +
     z_degree +
-    "<br />" +
-    "hoge: " +
-    hoge;
+    "<br />";
 }, 10);
 
 // function setup() {
